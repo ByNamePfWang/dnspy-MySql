ca62e376412eaa758f7936b18de9a4938031ac7a
pfwang <pf121x@163.com>
2020-11-09 16:44:31 +0800

    提交

--- HeatMeterPrePaySelfHelp/HeatMeterPrePaySelfHelp/Util/DbUtil.cs
+++ HeatMeterPrePaySelfHelp/HeatMeterPrePaySelfHelp/Util/DbUtil.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SQLite;
 using System.IO;
-using HeatMeterPrePay.exception;
 using HeatMeterPrePaySelfHelp.util;
 using MySql.Data.MySqlClient;
 
